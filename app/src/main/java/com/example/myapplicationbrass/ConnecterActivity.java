768ccf52d6edb014487183db24e5cc257efadf60
Etienne <etienne.gunslay@gmail.com>
2023-03-30 10:25:47 +0200

    Start

---
+++ app/src/main/java/com/example/myapplicationbrass/ConnecterActivity.java
@@ -0,0 +1,12 @@
+package com.example.myapplicationbrass;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import org.jetbrains.annotations.Nullable;
+
+public final class ConnecterActivity extends AppCompatActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_connecter);
+    }
+}
---
+++ app/src/main/java/com/example/myapplicationbrass/ModificationActivity.java
@@ -0,0 +1,12 @@
+package com.example.myapplicationbrass;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import org.jetbrains.annotations.Nullable;
+
+public final class ModificationActivity extends AppCompatActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_modif_info);
+    }
+}
